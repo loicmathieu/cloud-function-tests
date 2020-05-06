fb6d4d702282d8da7a5b8f7345b9783db6f4fc9e
Loïc Mathieu <loikeseke@gmail.com>
2020-05-06 12:57:53 +0200

    Hello World

---
+++ helloworld/src/main/java/Example.java
@@ -0,0 +1,14 @@
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class Example implements HttpFunction {
+    @Override
+    public void service(HttpRequest request, HttpResponse response)
+            throws IOException {
+        var writer = response.getWriter();
+        writer.write("Hello world!");
+    }
+}
